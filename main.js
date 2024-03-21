48d15cb264a90fe6758a3233de6324428498b41c
laurauniibk <l.eddelbuettel@hotmail.com>
2024-03-21 17:45:03 +0100

    defer eingefügt im script header und neue Koordinaten

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([45.873466, 11.799173], 13);
+var map = L.map('map').setView([-38.684444, 176.070833], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([45.873466, 11.799173]).addTo(map);
-marker.bindPopup("<strong>Monte Grappa</strong>").openPopup();
+var marker = L.marker([-38.684444, 176.070833]).addTo(map);
+marker.bindPopup("<strong>Taupo</strong>").openPopup();
