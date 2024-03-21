a23561be0d03eca6093b34648dc8fc6b7709f08d
laurauniibk <l.eddelbuettel@hotmail.com>
2024-03-21 17:47:53 +0100

    Ausschnitt Karte

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([-38.684444, 176.070833], 13);
+var map = L.map('map').setView([-38.684444, 176.070833], 9);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
