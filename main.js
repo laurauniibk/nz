8e521d96a96dfbf7cb1db5aaf3818fcc36684d40
laurauniibk <l.eddelbuettel@hotmail.com>
2024-04-11 16:06:42 +0200

    Let zoom und lat und lng

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-let map = L.map('map').setView([-38.684444, 176.070833], 9);
+let lat = -38.684444;
+let lng = 176.070833;
+let zoom = 9
+
+let map = L.map('map').setView([lat, lng], zoom);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-let marker = L.marker([-38.684444, 176.070833]).addTo(map);
+let marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<strong>Taupo</strong>").openPopup();
