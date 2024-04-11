59615a36a476b03b1fe464344f51521f137ea99e
laurauniibk <l.eddelbuettel@hotmail.com>
2024-04-11 15:55:50 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([-38.684444, 176.070833], 9);
+let map = L.map('map').setView([-38.684444, 176.070833], 9);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([-38.684444, 176.070833]).addTo(map);
+let marker = L.marker([-38.684444, 176.070833]).addTo(map);
 marker.bindPopup("<strong>Taupo</strong>").openPopup();
